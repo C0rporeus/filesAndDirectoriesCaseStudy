99c39d915aeb1af20504645624897be6ba4b086d
Yonathan Gutierrez <ygutierrez@cognos.com.co>
2022-02-26 18:48:52 -0500

    changes in use directory and write files data

--- Program.cs
+++ Program.cs
@@ -1,20 +1,54 @@
 ﻿using System.IO;
 using System.Collections.Generic;
+using Newtonsoft.Json; 
 
-var salesFiles = FindFiles("stores");
+var currentDirectory = Directory.GetCurrentDirectory();
+var storesDirectory = Path.Combine(currentDirectory, "stores");
 
-foreach (var file in salesFiles) {
+var salesTotalDir = Path.Combine(currentDirectory, "salesTotalDir");
+Directory.CreateDirectory(salesTotalDir);
+
+var salesFiles = FindFiles(storesDirectory);
+var salesJson = File.ReadAllText($"stores{Path.DirectorySeparatorChar}201{Path.DirectorySeparatorChar}sales.json");
+var salesData = JsonConvert.DeserializeObject<SalesTotal>(salesJson);
+
+Console.WriteLine(salesData?.Total);
+
+var data = JsonConvert.DeserializeObject<SalesTotal>(salesJson);
+
+File.WriteAllText(Path.Combine(salesTotalDir, "totals.txt"), String.Empty);
+File.AppendAllText($"salesTotalDir{Path.DirectorySeparatorChar}totals.txt", $"{data?.Total}{Environment.NewLine}");
+File.WriteAllText($"salesTotalDir{Path.DirectorySeparatorChar}totals.txt", data?.Total.ToString());
+
+
+
+foreach (var file in salesFiles)
+{
   Console.WriteLine(file);
 }
 
-IEnumerable<string> FindFiles(string folderName) {
+IEnumerable<string> FindFiles(string folderName)
+{
   List<string> salesFiles = new List<string>();
   var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
 
-  foreach (var file in foundFiles) {
-    if (file.EndsWith("sales.json")) {
+  foreach (var file in foundFiles)
+  {
+    if (file.EndsWith("sales.json"))
+    {
       salesFiles.Add(file);
     }
   }
   return salesFiles;
+}
+
+string fileName = $"stores{Path.DirectorySeparatorChar}201{Path.DirectorySeparatorChar}sales{Path.DirectorySeparatorChar}sales.json";
+
+FileInfo info = new FileInfo(fileName);
+
+Console.WriteLine($"Full Name: {info.FullName}{Environment.NewLine}Directory: {info.Directory}{Environment.NewLine}Extension: {info.Extension}{Environment.NewLine}Create Date: {info.CreationTime}");
+
+class SalesTotal
+{
+  public double Total { get; set; }
 }
