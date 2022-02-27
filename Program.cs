4b7d928da81172c0d7118e18cabb4e3db3dfc67c
Yonathan Gutierrez <ygutierrez@cognos.com.co>
2022-02-26 19:03:29 -0500

    all values in a file result

--- Program.cs
+++ Program.cs
@@ -4,23 +4,20 @@ using Newtonsoft.Json;
 
 var currentDirectory = Directory.GetCurrentDirectory();
 var storesDirectory = Path.Combine(currentDirectory, "stores");
-
 var salesTotalDir = Path.Combine(currentDirectory, "salesTotalDir");
 Directory.CreateDirectory(salesTotalDir);
-
 var salesFiles = FindFiles(storesDirectory);
 var salesJson = File.ReadAllText($"stores{Path.DirectorySeparatorChar}201{Path.DirectorySeparatorChar}sales.json");
 var salesData = JsonConvert.DeserializeObject<SalesTotal>(salesJson);
+var salesTotal = CalculateSalesTotal(salesFiles);
 
 Console.WriteLine(salesData?.Total);
 
 var data = JsonConvert.DeserializeObject<SalesTotal>(salesJson);
 
-File.WriteAllText(Path.Combine(salesTotalDir, "totals.txt"), String.Empty);
-File.AppendAllText($"salesTotalDir{Path.DirectorySeparatorChar}totals.txt", $"{data?.Total}{Environment.NewLine}");
-File.WriteAllText($"salesTotalDir{Path.DirectorySeparatorChar}totals.txt", data?.Total.ToString());
-
-
+// File.WriteAllText(Path.Combine(salesTotalDir, "totals.txt"), String.Empty);
+File.AppendAllText(Path.Combine(salesTotalDir, "totals.txt"), $"{salesTotal}{Environment.NewLine}");
+// File.WriteAllText($"salesTotalDir{Path.DirectorySeparatorChar}totals.txt", data?.Total.ToString());
 
 foreach (var file in salesFiles)
 {
@@ -42,6 +39,26 @@ IEnumerable<string> FindFiles(string folderName)
   return salesFiles;
 }
 
+double CalculateSalesTotal(IEnumerable<string> salesFiles)
+{
+    double salesTotal = 0;
+
+    // Loop over each file path in salesFiles
+    foreach (var file in salesFiles)
+    {      
+        // Read the contents of the file
+        string salesJson = File.ReadAllText(file);
+
+        // Parse the contents as JSON
+        SalesData? data = JsonConvert.DeserializeObject<SalesData?>(salesJson);
+
+        // Add the amount found in the Total field to the salesTotal variable
+        salesTotal += data?.Total ?? 0;
+    }
+
+    return salesTotal;
+}
+
 string fileName = $"stores{Path.DirectorySeparatorChar}201{Path.DirectorySeparatorChar}sales{Path.DirectorySeparatorChar}sales.json";
 
 FileInfo info = new FileInfo(fileName);
@@ -52,3 +69,6 @@ class SalesTotal
 {
   public double Total { get; set; }
 }
+
+record SalesData (double Total);
+
